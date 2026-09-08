c77ed5c1c330740c0078c73a8da84d212398c5e0
szeshuktung2627 <szeshuksst@gmail.com>
2026-09-08 06:07:11 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -2,4 +2,4 @@ skill_completed = "Python Syntax "
 exercises_completed = 13
 point_per_exercise = 5
 point_total = 100
-point_total = exercises_completed * points_per_exer
+point_total = exercises_completed * points_per_exercise=point_total
