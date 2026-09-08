b4708422c239f3721298bf8648f4c4e6888d9534
szeshuktung2627 <szeshuksst@gmail.com>
2026-09-08 02:33:54 +0000

    [skip actions] lab/lab01/ch01_t04_handling_errors.py

--- lab/lab01/ch01_t04_handling_errors.py
+++ lab/lab01/ch01_t04_handling_errors.py
@@ -1,4 +1,4 @@
 #print("How do you make a hot dog stand?')
 # print(You take away its chair!)
 print("Mismatched quotes will cause a SyntaxError")
-print(Without quotes will cause a NameError")
+print("Without quotes will cause a NameError")
