c87d398648b57d3559c29da98a9cdb3a84cdeda6
szeshuktung2627 <szeshuksst@gmail.com>
2026-09-08 06:03:02 +0000

    [skip actions] lab/lab01/ch01_t13_value_error.py

--- lab/lab01/ch01_t13_value_error.py
+++ lab/lab01/ch01_t13_value_error.py
@@ -1,11 +1,2 @@
 float_1 = 0.25
 float_2 = 40.0
-# Create string product containing the result of multiplication
-product = str(float_1 * float_2)
-
-# Create big_string with the product inserted
-big_string = "The product was " + product
-
-# Output for verification
-print(product)
-print(big_string)
