d9f4f4fa0dcff8af43199e3664e04c589300ddba
szeshuktung2627 <szeshuksst@gmail.com>
2026-09-08 05:58:19 +0000

    [skip actions] lab/lab01/ch01_t12_boolean.py

--- lab/lab01/ch01_t12_boolean.py
+++ lab/lab01/ch01_t12_boolean.py
@@ -1,5 +1,5 @@
 # Hi! I'm Maria and I live in script.py.
 # I'm an expert Python coder.
 # I'm 21 years old and I plan to program cool stuff forever.
-print(age_is_12)
-print(name_is_maria)
+
+name_is_maria
