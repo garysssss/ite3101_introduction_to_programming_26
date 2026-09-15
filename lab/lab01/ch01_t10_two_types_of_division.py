51905fcc16fd1841bc10c585e97503ffb0e9ed49
szeshuktung2627 <szeshuksst@gmail.com>
2026-09-15 02:01:08 +0000

    [skip actions] lab/lab01/ch01_t10_two_types_of_division.py

--- lab/lab01/ch01_t10_two_types_of_division.py
+++ lab/lab01/ch01_t10_two_types_of_division.py
@@ -1,7 +1,7 @@
 cucumber = [100]
 num_people = [6]
 
-whole_cucumber_per_person = cucumber//Num_people
+    hole_cucumber_per_person = cucumber//Num_people
 print('whole_cucumber_per_person')
 float_cucumber_per_person = cucumber/Num_people
 print('float_cucumer_per_person')
