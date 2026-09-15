41b4bd4ce4809fba02d74b7adf33cd9c131ac885
szeshuktung2627 <szeshuksst@gmail.com>
2026-09-15 02:06:58 +0000

    [skip actions] lab/lab02/ch02_t02_practice.py

--- lab/lab02/ch02_t02_practice.py
+++ lab/lab02/ch02_t02_practice.py
@@ -1,7 +1,7 @@
-# Assign your variables belo-	Set caesar to "Graham"
+-	Set caesar to "Graham"
 -	Set praline to "John"
 -	Set viking to "Teresa"
-w, each on its own line!
+
 
 
 # Put your variables above this line, and uncomment the following 3 line of codes.
