563541f5f838c55fe2d7e6a078023ae19131c573
szeshuktung2627 <szeshuksst@gmail.com>
2026-09-22 03:02:20 +0000

    lab/lab04/ch04_t04_how_tables_have_turned.py

--- lab/lab04/ch04_t04_how_tables_have_turned.py
+++ lab/lab04/ch04_t04_how_tables_have_turned.py
@@ -6,7 +6,8 @@ bool_one = 3 < 5  # We already did this one for you!
 # Make me false!
 bool_two = 1 > 2    
 
-# Make me true!bool_three = 1+
+# Make me true!
+bool_three = 1==1
 
 # Make me false!
 bool_four = 2 < 1
