a9479453cfc2ef5eedaaaed162c9ec8e6d783615
szeshuktung2627 <szeshuksst@gmail.com>
2026-09-22 03:06:06 +0000

    [skip actions] lab/lab04/ch04_t06_and.py

--- lab/lab04/ch04_t06_and.py
+++ lab/lab04/ch04_t06_and.py
@@ -2,8 +2,8 @@ bool_one = 10>20 and 5==9
 
 bool_two = 2+2 ==5 and 10>3 
 
-bool_three = 6>1 and 7< 2
+bool_three = 6>1 and 7<2
 
-bool_four = 3< 0 and 4 > 1
+bool_four = 3<0 and 4 >1
 
 bool_five = 8>2 and 5 !=10
